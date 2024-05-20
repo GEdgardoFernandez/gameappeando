dc895a9c3139c1a41dacfe2cacd6f0d5b79da88e
GEdgardoFernandez <gecozzi87@gmail.com>
2024-05-20 13:02:44 -0300

    se agrego el paginado

--- client/src/components/GamesCards/GamesCards.jsx
+++ client/src/components/GamesCards/GamesCards.jsx
@@ -1,5 +1,5 @@
 import style from './GamesCards.module.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CardGame from '../CardGame/CardGame';
 import Pagination from '../Pagination/Pagination';
@@ -13,18 +13,30 @@ export default function GamesCards() {
   }, [dispatch]);
 
   const Allgames = useSelector(state => state.videogames); // Accede al estado de los juegos desde Redux
-  const games = Allgames.slice(0, 15); // Obtiene solo los primeros 15 elementos de la lista de juegos
+    // Calcula los juegos actuales
+    const [currentPage, setCurrentPage] = useState(1);
+    const gamesPerPage = 15;
+    const indexOfLastGame = currentPage * gamesPerPage;
+    const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+   
+  const games = Allgames.slice(indexOfFirstGame, indexOfLastGame); // Obtiene solo los primeros 15 elementos de la lista de juegos
   console.log(games)
-  if (games.length === 0){
+  
+ 
+
+  // Cambia de página
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+  if (games.length === 0) {
 
     return <div>
       <Loading />
     </div>
   }
   return (
+    
     <div className={style.containerCards}>
-       {/* Verificar si la lista de juegos está definida antes de mapearla */}
-       {games?.map(g => (
+      {/* Verificar si la lista de juegos está definida antes de mapearla */}
+      {games?.map(g => (
         <CardGame
           key={g.id} // Asegúrate de incluir una clave única para cada elemento en el array mapeado
           id={g.id}
@@ -33,6 +45,13 @@ export default function GamesCards() {
           genre={g.genres.join(', ')}
         />
       ))}
+      <Pagination
+        gamesPerPage={gamesPerPage}
+        totalGames={Allgames.length}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
     </div>
+
   );
 }
